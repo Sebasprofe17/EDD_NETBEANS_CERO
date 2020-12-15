2367e5180cdea272fd509d244d3a083f7477cde5
sebasprofe17 <sebasprofe17@gmail.com>
2020-12-15 17:37:10 +0100

    Crear projecte

---
+++ src/edd_netbeans_cero/EDD_NETBEANS_CERO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd_netbeans_cero;
+
+/**
+ *
+ * @author sebbre
+ */
+public class EDD_NETBEANS_CERO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
